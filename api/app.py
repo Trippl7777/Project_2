ff936f136f3120b955f61208536a611dced9f96d
Julian Freeman <jofreeman1014@gmail.com>
2019-10-19 13:33:59 -0400

    Update app.py

--- api/app.py
+++ api/app.py
@@ -38,8 +38,18 @@ class CrimeWeather(db.Model):
 
 # create route that renders index.html template
 @app.route("/")
-def home():
-    return render_template("index.html")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api for a test api statement.<br/>"
+        f"/api/[INSERT CITY HERE] for a test api statement specifying city name."
+    )
+
+
+# @app.route("/")
+# def home():
+#     return render_template("index.html")
 
 
 # Query the database and send the jsonified results
@@ -61,18 +71,8 @@ def send():
 @app.route("/api")
 def crimeweather():
     result = db.session.query(CrimeWeather.city,CrimeWeather.code,CrimeWeather.startdate,CrimeWeather.starttime,
-    CrimeWeather.latitude,CrimeWeather.longitude,CrimeWeather.mapping,CrimeWeather.weather).first()#.all()
-
-    # Uncomment these and comment the duplicates below if .all() is selected
-    # city = [result[0] for result in results]
-    # code = [result[1] for result in results]
-    # date = [result[2] for result in results]
-    # time = [result[3] for result in results]
-    # lat = [result[4] for result in results]
-    # lon = [result[5] for result in results]
-    # mapping = [result[6] for result in results]
-    # weather = [result[7] for result in results]
-
+    CrimeWeather.latitude,CrimeWeather.longitude,CrimeWeather.mapping,CrimeWeather.weather).first()
+    
     city = result[0]
     code = result[1]
     date = result[2]
@@ -101,7 +101,7 @@ def crimeweather():
 
     return jsonify(crimeweather_data)
 
-@app.route("/api/<city>")
+@app.route("/api/<city>") # City filter test path
 def crimeweather_state(city):
     result = db.session.query(CrimeWeather.city,CrimeWeather.code,CrimeWeather.startdate,CrimeWeather.starttime,
     CrimeWeather.latitude,CrimeWeather.longitude,CrimeWeather.mapping,CrimeWeather.weather).filter(CrimeWeather.city == city).first()#.all()
