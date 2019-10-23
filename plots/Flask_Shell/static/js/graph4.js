a62c038b7cc0d2218dd2cb108d9bfdca7ab60c3c
Jack <trippl7777@gmail.com>
2019-10-23 12:33:28 -0400

    Heatmap

--- plots/Flask_Shell/static/js/graph4.js
+++ plots/Flask_Shell/static/js/graph4.js
@@ -1,86 +1,61 @@
+var URL = `/heatmap`;
+
 getdata();
-// Initiate Sunburst draw
-function init() {
-  getdata();
-}
 
-// Define input variables
-var par, lbl, ids, val
-// var weat;
 
-// If window is resized, resize chart accordingly
-window.onresize = function() {
-  var area = d3.select('svg');
+var map = L.map('map').setView([38, -89], 4);
 
-  if (!area.empty()) {
-      area.remove();
-          draw();
-  }
-};
+var baseLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
-//Pull Data for the sub-api setup
 function getdata() {
 
-  var url = `/heatmap`;
-  d3.json(url).then(function(sunnyb) {
+  d3.json(URL).then(function(heat) {
 
-  ids = sunnyb.ids
-  lbl = sunnyb.labels
-  par = sunnyb.parents
-  val = (sunnyb.values).map(Number)
-  weat = sunnyb.weather
+    lat = parseFloat(heat.lat)
+    lng = parseFloat(heat.lng)
 
-  //Ignore this if still uncommented. Attempt to add checkboxes to already interatve chart
+    // console.log(lat);
+// new code for GeoJson
 
-  // console.log("IDS", ids, "Labels", lbl,"Parents", par, "Values", val);
-  // var checks = d3.select("#checkers");
-  // checks.html('')
-  // console.log("Working")
-  
-  // for(i=0; i<weat.length; i++) {
-  //   checks.append("li")
-  //     .append("input")
-  //     .attr("type","checkbox")
-  //     .property("value", weat[i])
-  //     .attr("onchange","act(this.value)")
-  //     .attr("checked")
-  //     .text(weat[i])
-  // }
+    var jsonFeatures = [];
 
-  draw();
-})
-}
+    var feature = {type: 'Feature',
+      properties: heat,
+      geometry: {
+        type: 'Point',
+        coordinates: [lng,lat]
+      }
+    };
 
-// function init(weat) {
+    jsonFeatures.push(feature);
 
-//   };
+    var geoJson = { type: 'FeatureCollection', features: jsonFeatures };
 
-// function act(value) {
-//   console.log(value)
-// }
- 
-//Here comes the sun doo, doo, doo
-function draw() {
-  
-//Define sunburst data
-var data = [
-    {
-      type: "sunburst",
-      ids: ids,
-      labels: lbl,
-      parents: par,
-      values: val,
-      maxdepth: 3,
-      branchvalues: 'total',
+    console.log(geoJson);
+
+    L.geoJson(geoJson).addTo(map);
+
+    // Add Heat Map
+
+    var heatArray = [];
+
+    for (var i = 0; i < geoJson.length; i++) {
+      var location = geoJson[i].location;
+
+      if (location) {
+        heatArray.push([location.coordinates[1], location.coordinates[0]]);
+      }
     }
-  ];
 
-  var layout = {
-    width: document.getElementById("sunburst").offsetWidth,
-    height: document.getElementById("sunburst").offsetWidth*.8,
-    sunburstcolorway:["#866d4f","#c83803","#002244","#b0a3bc","#a71930"],
-    extendsunburstcolorway: true
-  };  
+    var heat = L.heatLayer(heatArray, {
+      radius: 20,
+      blur: 35
+    }).addTo(myMap);
 
-Plotly.newPlot("sunburst", data, layout, {responsive: true});
-}
+  });
+
+
+
+  };
